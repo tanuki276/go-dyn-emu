3fe935b76a98680d8d2cbbb97eb5e13b09e92ccb
blowa <light06120602@gmail.com>
2025-12-11 23:29:54 +0900

    main.go の更新

--- cmd/dynamo-emulator/main.go
+++ cmd/dynamo-emulator/main.go
@@ -1,28 +1,29 @@
+
 package main
 
 import (
-	"log"
-	"os"
+    "log"
+    "os"
 
-	"Emulator-fr-virtuelle-Datenbanken-gobes/pkg/api"
-	"Emulator-fr-virtuelle-Datenbanken-gobes/pkg/core"
+    "go-dyn-emu/pkg/api"
+    "go-dyn-emu/pkg/core"
 )
 
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	
-	db, err := core.NewDatabase()
-	if err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
-	}
-	defer db.Close()
-	
-	server := api.NewServer(db)
-	
-	addr := ":8000"
-	if p := os.Getenv("PORT"); p != "" {
-		addr = ":" + p
-	}
-	
-	server.Start(addr)
+    log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+    db, err := core.NewDatabase()
+    if err != nil {
+        log.Fatalf("Failed to initialize database: %v", err)
+    }
+    defer db.Close()
+
+    server := api.NewServer(db)
+
+    addr := ":8000"
+    if p := os.Getenv("PORT"); p != "" {
+        addr = ":" + p
+    }
+
+    server.Start(addr)
 }
