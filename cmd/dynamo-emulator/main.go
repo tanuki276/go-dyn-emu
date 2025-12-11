8fd192e9ba4fcaeecd39359b92a84dc9fa831dcd
blowa <light06120602@gmail.com>
2025-12-11 23:02:00 +0900

    main.go の更新

--- cmd/dynamo-emulator/main.go
+++ cmd/dynamo-emulator/main.go
@@ -1,55 +1,28 @@
 package main
 
 import (
-        "os"
-        "os/signal"
-        "syscall"
-        "fmt"
+	"log"
+	"os"
+
+	"Emulator-fr-virtuelle-Datenbanken-gobes/pkg/api"
+	"Emulator-fr-virtuelle-Datenbanken-gobes/pkg/core"
 )
 
 func main() {
-        if len(os.Args) > 1 {
-                switch os.Args[1] {
-                case "snapshot":
-                        if len(os.Args) < 4 {
-                                fmt.Println("Usage: go run . snapshot [create|load] <name>")
-                                os.Exit(1)
-                        }
-
-                        db, err := NewDatabase()
-                        if err != nil {
-                                os.Exit(1)
-                        }
-
-                        subcommand := os.Args[2]
-                        name := os.Args[3]
-
-                        if subcommand == "create" {
-                                if err := db.CreateSnapshot(name); err != nil {
-                                        os.Exit(1)
-                                }
-                        } else if subcommand == "load" {
-                                if err := db.LoadSnapshot(name); err != nil {
-                                        os.Exit(1)
-                                }
-                        } else {
-                                fmt.Println("Invalid snapshot subcommand. Use 'create' or 'load'.")
-                                os.Exit(1)
-                        }
-                        os.Exit(0)
-                }
-        }
-
-        db, err := NewDatabase()
-        if err != nil {
-                os.Exit(1)
-        }
-        defer db.Close()
-
-        server := NewServer(db)
-        server.ListenAndServe(":8000")
-
-        quit := make(chan os.Signal, 1)
-        signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-        <-quit
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	
+	db, err := core.NewDatabase()
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	defer db.Close()
+	
+	server := api.NewServer(db)
+	
+	addr := ":8000"
+	if p := os.Getenv("PORT"); p != "" {
+		addr = ":" + p
+	}
+	
+	server.Start(addr)
 }
